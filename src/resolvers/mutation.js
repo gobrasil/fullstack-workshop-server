cbd1d5ea325781c20e90fc0aa73bd7d58b5cf88e
Peggy Rayzis <peggyrayzis@gmail.com>
2018-06-26 00:06:55 -0500

    Clarified mutation instructions
    Added login mutation

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -5,6 +5,10 @@ fieldName: (obj, args, context, info) => result;
 
 Check models/movie for the data fetching functions you'll need to complete the exercise.
 Refer to your schema if you're unsure what to return from the resolvers.
+
+For toggleLike, only authenticated users can toggle a like. Try grabbing the user
+off the context and throw an error if there is no user.
+
 Good luck!
 */
 
@@ -15,6 +19,6 @@ module.exports = {
       await models.movie.toggleMovieLike({ id, user });
       return models.movie.getMovieById(id);
     },
-    login: () => 'yo',
+    login: (_, { email }) => new Buffer(email).toString('base64'),
   },
 };
